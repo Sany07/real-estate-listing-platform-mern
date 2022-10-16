01ee0b47eadd8b33a6a85bcc1cff4226dad3d172
Ahasan Habib Sany <sany.baust@gmail.com>
2022-10-16 14:55:28 +0600

    prepare for backend deploy

--- client/README.md
+++
@@ -1,1 +0,0 @@
-#property-listing-platform
--- server/utils/generateJwtToken.js
+++ server/utils/generateJwtToken.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 // @desc Generate JWT
-const generateJwtToken = (id) => {
+const generateJwtToken = (id) => { 
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_Expires,
   });
